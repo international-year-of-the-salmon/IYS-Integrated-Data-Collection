a78f54bef46660a7a69882904cad12b0ba7b361f
Brett Johnson <brettthomasjohnson@gmail.com>
2022-08-17 15:06:32 -0700

    add Raw Spirit planned station coordinates to event file and updated map

--- scripts/map_scripts/bathymetric_station_map.R
+++ scripts/map_scripts/bathymetric_station_map.R
@@ -6,10 +6,10 @@ library(readxl)
 library(here)
 
 # Edit which vessels, years, or sampling event types you would like to plot
-vessel <- c("TINRO", "NW", "Shimada", "Franklin", "Kaganovsky", "Legacy")
+vessel <- c("TINRO", "NW", "Shimada", "Franklin", "Kaganovsky", "Legacy", "Raw_Spirit")
 sampling_year <- c(2022)
-#event_types could include "Trawl", "CTD" or "CTD-Rosette"
-event_types <- c("Trawl")
+#event_types could include "Trawl", "CTD" or "CTD-Rosette", "Gillnet"
+event_types <- c("Trawl", "Gillnet")
 
 iys_data <- read_excel(list.files(pattern = "\\.xlsx$"), 
                        sheet = "Sampling_Events") |> 
@@ -17,7 +17,8 @@ iys_data <- read_excel(list.files(pattern = "\\.xlsx$"),
          year %in% sampling_year,
          event_type %in% event_types) |> 
   rename("Vessel" = "vessel_name_abbr") |> 
-  mutate(Vessel = ifelse(Vessel == "NW", "NW Explorer", Vessel))
+  mutate(Vessel = ifelse(Vessel == "NW", "NW Explorer", Vessel),
+         Vessel = ifelse(Vessel == "Raw_Spirit", "Raw Spirit", Vessel))
 
 bathymetric_station_map <- 
   basemap(data = iys_data,  rotate = TRUE, bathymetry = TRUE, lon.interval = 3) +
@@ -32,3 +33,10 @@ bathymetric_station_map <-
 bathymetric_station_map
 
 ggsave(here("maps", "bathymetric_station_map.png"))
+
+polygon_map <- basemap(data = iys_data, rotate = TRUE) +
+  geom_spatial_polygon(data = iys_data, aes(x = longitude_start_decdeg,
+                                           y = latitude_start_decdeg), 
+                                           alpha = 0.5)
+
+polygon_map
